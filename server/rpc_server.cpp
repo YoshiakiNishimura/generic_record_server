e401962f81b52b0cb636ef6c07179189f2fe0277
Yoshiaki Nishimura <nishimura@nautilus-technologies.com>
2025-08-06 20:21:35 +0900

    rename

---
+++ server/rpc_server.cpp
@@ -0,0 +1,62 @@
+#include <iostream>
+#include <memory>
+#include <string>
+
+#include "greeter.grpc.pb.h"
+#include "greeter.pb.h"
+#include <grpcpp/grpcpp.h>
+
+using grpc::Server;
+using grpc::ServerBuilder;
+using grpc::ServerContext;
+using grpc::Status;
+using myapi::Byer;
+using myapi::Greeter;
+
+class GreeterServiceImpl final : public Greeter::Service {
+    Status SayHello(
+        ServerContext* context, const StringValue* request, StringValue* reply) override {
+        std::string prefix("Hello ");
+        reply->set_value(prefix + request->value());
+        return Status::OK;
+    }
+    Status IntAddOne(
+        ServerContext* context, const Int32Value* request, Int32Value* reply) override {
+        int sum = request->value() + 1;
+        reply->set_value(sum);
+        return Status::OK;
+    }
+};
+
+class ByerServiceImpl final : public Byer::Service {
+    Status SayWorld(
+        ServerContext* context, const StringValue* request, StringValue* reply) override {
+        std::string prefix("World ");
+        reply->set_value(prefix + request->value());
+        return Status::OK;
+    }
+    Status DecDecimal(ServerContext* context, const tsurugidb::udf::value::Decimal* request,
+        tsurugidb::udf::value::LocalTime* reply) override {
+        return Status::OK;
+    }
+};
+
+void RunServer() {
+    std::string server_address("0.0.0.0:50051");
+    GreeterServiceImpl greeter_service;
+    ByerServiceImpl byer_service;
+
+    ServerBuilder builder;
+    builder.AddListeningPort(server_address, grpc::InsecureServerCredentials());
+    builder.RegisterService(&greeter_service);
+    builder.RegisterService(&byer_service);
+    std::unique_ptr<Server> server(builder.BuildAndStart());
+    std::cout << "Server listening on " << server_address << std::endl;
+
+    server->Wait();
+}
+
+int main(int argc, char** argv) {
+    RunServer();
+    return 0;
+}
